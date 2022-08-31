3c11a85395057c9687ee250df418cf222af6b876
unknown <a.zakharyan1990@gmail.com>
2022-08-31 21:49:20 +0300

    Task3.2 uploaded

---
+++ Task3.2.py
@@ -0,0 +1,4 @@
+Casino_blacklist = {'John Ben', 'Peter Moll', 'Ivan Ivanov'}
+poker_blacklist = {'Ivan Ivanov', 'Marta Brown', 'Nell Barton'}
+Alcohol_Blacklist = {'Petr Petrov', 'Ivan Ivanov', 'Vasiliy Sidorov'}
+print(Casino_blacklist.intersection(poker_blacklist, Alcohol_Blacklist))
